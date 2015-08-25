cceafb80e6788c93eca915cd55dbfdff079946b4
Jason Yoon <jasony0206@gmail.com>
2015-08-25 16:44:32 -0700

    Add logger

--- src/main/java/com/jasonyoon/mvnapp/Application.java
+++ src/main/java/com/jasonyoon/mvnapp/Application.java
@@ -1,5 +1,7 @@
 package com.jasonyoon.mvnapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,10 +15,15 @@ import java.util.Arrays;
 @SpringBootApplication
 public class Application {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+
+        LOG.info("Running application");
+
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        LOG.info("Inspecting the beans provided by Spring Boot:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
