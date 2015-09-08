b2c1964f582093577dd70c4f34cf806f024604e2
Jason Yoon <jasony0206@gmail.com>
2015-09-08 14:18:57 -0700

    Add Jackson dependency and ResponseBody annotation

--- src/main/java/com/jasonyoon/mvnapp/controller/JsonController.java
+++ src/main/java/com/jasonyoon/mvnapp/controller/JsonController.java
@@ -5,21 +5,23 @@ import com.jasonyoon.mvnapp.util.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by jason on 8/25/15.
  */
 
-@RestController
+@Controller
 public class JsonController {
 
     private static final Logger LOG = LoggerFactory.getLogger(JsonController.class);
 
     @RequestMapping(value = "/json", method = RequestMethod.GET)
+    @ResponseBody
     public ModelMap jsonController() {
         LOG.info("Entering /json endpoint to see an example of returning a JSON object");
 
